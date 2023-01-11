22f516416beefd8b4aa08b2ae2d8840983fb541c
SACHIN_H_S <46858566+SACHINHS04@users.noreply.github.com>
2023-01-11 11:10:10 +0530

    Create Grammar_Corrector_and_Suggestion_using_GPT-3_app.py

---
+++ Grammar_Corrector_and_Suggestion_using_GPT-3_app.py
@@ -0,0 +1,19 @@
+import openai
+import streamlit as st
+
+def correct_passage():
+    api_key = st.text_input("Please enter OpenAI API key", "")
+    openai.api_key = api_key
+    passage = st.text_area("Enter the passage you want to correct", "")
+
+    # Use GPT-3 to correct the grammar in the passage
+    prompt = (f"Please correct the grammar in the following passage and suggest a better version: {passage}")
+    corrected_passage = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=2048, n =1,stop=None,temperature=0.5)
+
+    # Display the corrected passage
+    st.write("Corrected Passage :")
+    st.write(corrected_passage.choices[0].text)
+
+if __name__=="__main__":
+    st.title("Grammar Corrector and Suggestion using GPT-3")
+    correct_passage()
